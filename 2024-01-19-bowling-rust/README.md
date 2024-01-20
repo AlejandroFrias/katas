fcf3067fcb9d46e8fe06cd2c655564fa9e12ab27
Alejandro Frias <3598338+AlejandroFrias@users.noreply.github.com>
2024-01-20 11:49:53 -0600

    rust read me

---
+++ 2024-01-19-bowling-rust/README.md
@@ -0,0 +1,12 @@
+Install Rust: https://www.rust-lang.org/tools/install
+
+I'm using rustup and not brew.
+
+Run tests:
+```
+cargo watch
+```
+Or in watch mode:
+```
+cargo watch test
+```
--- 2024-01-19-bowling-rust/src/lib.rs
+++ 2024-01-19-bowling-rust/src/lib.rs
@@ -1,13 +1,8 @@
-pub fn bowling(rolls: & [i32]) -> i32 {
-    let mut score: i32 = rolls.iter().sum();
-    let mut bonus: i32 = if rolls.len() > 2 {
-        5
-    } else {
-        0
-    };
+pub fn bowling(rolls: &[i32]) -> i32 {
+    let score: i32 = rolls.iter().sum();
+    let bonus: i32 = if rolls.len() > 2 { 5 } else { 0 };
 
-     return score + bonus;
-
+    return score + bonus;
 }
 
 #[cfg(test)]
@@ -16,43 +11,43 @@ mod tests {
 
     #[test]
     fn no_rolls() {
-        let result = bowling(& []);
+        let result = bowling(&[]);
         assert_eq!(result, 0);
     }
 
     #[test]
     fn one_roll_one_pin() {
-        let result = bowling(& [1]);
+        let result = bowling(&[1]);
         assert_eq!(result, 1);
     }
 
     #[test]
     fn one_roll_two_pins() {
-        let result = bowling(& [2]);
+        let result = bowling(&[2]);
         assert_eq!(result, 2);
     }
 
     #[test]
     fn two_rolls_two_pins() {
-        let result = bowling(& [2,2]);
+        let result = bowling(&[2, 2]);
         assert_eq!(result, 4);
     }
 
     #[test]
     fn spare_bonus_five() {
-        let result = bowling(& [9,1,5]);
+        let result = bowling(&[9, 1, 5]);
         assert_eq!(result, 20);
     }
 
     #[test]
     fn double_spare() {
-        let result = bowling(& [9,1,5,5]);
+        let result = bowling(&[9, 1, 5, 5]);
         assert_eq!(result, 25);
     }
 
     #[test]
     fn spare_bonus_seven() {
-        let result = bowling(& [8,2,7]);
+        let result = bowling(&[8, 2, 7]);
         assert_eq!(result, 24);
     }
 }
